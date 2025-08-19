90452d891dfa57603b62e9c05e0dcb66f23bee14
Kenechukwu Nwakalo <nwakalokenechukwu@gmail.com>
2025-08-19 19:48:32 +0100

    removed throttle

--- Naviproai/navipro/src/app.module.ts
+++ Naviproai/navipro/src/app.module.ts
@@ -13,20 +13,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from './config/config.module';
 import { ConfigService } from './config/config.service';
 import { EmailModule } from './email/email.module';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule,
-    ThrottlerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => [{
-        ttl: config.throttleTtl,
-        limit: config.throttleLimit,
-      }],
-    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
@@ -45,9 +35,6 @@ import { APP_GUARD } from '@nestjs/core';
     EmailModule,
   ],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_GUARD,
-    useClass: ThrottlerGuard,
-  }],
+  providers: [AppService],
 })
 export class AppModule {}
