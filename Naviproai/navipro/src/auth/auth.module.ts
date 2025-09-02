f200a8633ebd7625551c68ac724d18e35ff25834
Kenechukwu Nwakalo <nwakalokenechukwu@gmail.com>
2025-09-02 16:41:04 +0100

    global export

--- Naviproai/navipro/src/auth/auth.module.ts
+++ Naviproai/navipro/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Global, Module, forwardRef } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
